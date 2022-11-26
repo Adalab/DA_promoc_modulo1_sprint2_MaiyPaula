268a48f1af8542153766918e8098cb0457ef953f
Marinaruizz <infomarinaruiz@gmail.com>
2022-11-26 14:26:14 +0100

    final26-11

--- SQL/pair_programming_consultas_CTEs.sql
+++ SQL/pair_programming_consultas_CTEs.sql
@@ -7,6 +7,8 @@ Extraer en una CTE todos los nombres de las compañias y los id de los clientes.
 Para empezar nos han mandado hacer una CTE muy sencilla el id del cliente y el 
 nombre de la compañia de la tabla Customers. */
 
+-- Creamos una CTE con la sintáxis WITH y AS dándola el nombre cliente y renombrando 
+-- las columnas. Extraemos la información solicitada de la tabla customers.
 
 WITH cte_cliente (id_cliente, empresa)
 AS ( 
@@ -19,6 +21,9 @@ FROM cte_cliente;
 /* EJERCICIO 2
 Ampliemos un poco la query anterior. En este caso, queremos un resultado similar 
 al anterior, pero solo queremos los que pertezcan a "Germany".*/
+
+-- Creamos una CTE dándola el nombre cliente_alemania y renombrando las columnas,
+-- extrayendo la información de la tabla customers, filtrando por el país Germany.
 
 WITH cte_cliente_alemania (id_cliente, empresa)
 AS ( 
@@ -36,6 +41,9 @@ En este caso queremos extraer todas las facturas que se han emitido a un cliente
 la compañia a la que pertenece.
 NOTA En este caso tendremos columnas con elementos repetidos(CustomerID, y Company Name). */
 
+-- Creamos una CTE con la información solicitada de la tabla customers, utilizando un 
+-- INNER JOIN para conectar y ampliar la información con la tabla orders, compartiendo 
+-- las dos el mismo id de cliente.
 
 WITH cte_cliente (id_cliente, empresa)
 AS ( 
@@ -52,6 +60,8 @@ Contad el número de facturas por cliente
 Mejoremos la query anterior. En este caso queremos saber el número de facturas emitidas por 
 cada cliente.*/
 
+-- Añadimos a la consulta un group by para que nos agrupe las facturas/orders por cliente.
+
 WITH cte_cliente (id_cliente, empresa)
 AS ( 
 	SELECT customer_id, company_name 
@@ -65,11 +75,33 @@ GROUP BY customer_id;
 
 
 /* EJERCICIO 5
-Cuál la cantidad media pedida de todos los productos ProductID.
+Cuál es la cantidad media pedida de todos los productos ProductID.
 Necesitaréis extraer la suma de las cantidades por cada producto y calcular la media.*/
 
+-- Con Id_producto como referencia del producto
+
+WITH suma_cantidad (id_producto, suma)
+AS (
+	SELECT product_id, SUM(quantity)
+    FROM order_details
+    GROUP BY product_id)
+SELECT id_producto, AVG(suma)
+FROM suma_cantidad
+GROUP BY id_producto;                                     
 
 
+-- Con Product_name como referencia del producto
+
+WITH suma_cantidad (id_producto, suma, numero_pedidos)
+AS (
+	SELECT product_id, SUM(quantity), COUNT(DISTINCT order_id)
+    FROM order_details
+    GROUP BY product_id)
+SELECT product_name, AVG(suma)
+FROM suma_cantidad
+INNER JOIN products
+ON suma_cantidad.id_producto = products.product_id
+GROUP BY numero_pedidos;
 
 
 
